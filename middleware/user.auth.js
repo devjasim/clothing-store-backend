df7889ae943fc9036ec2d0a995a89e39e93d7a04
devjasim <jasimfbd@gmail.com>
2022-05-27 23:33:54 +0600

    update backend

--- middleware/user.auth.js
+++ middleware/user.auth.js
@@ -6,16 +6,14 @@ const userAuth = async(req, res, next) => {
     if(req.headers?.authorization) {
       const token = req.headers.authorization.split(" ")[1];
       const isCustomAuth = token.length < 500;
-
+      
       let decodedData;
 
       if(token && isCustomAuth) {
         decodedData = jwt.verify(token, config.JWT_SECRET);
-
         req.userId = decodedData?.id;
       } else {
-        decodedData = json.decode(token);
-
+        decodedData = jwt.decode(token);
         req.userId = decodedData?.sub;
       }
 
