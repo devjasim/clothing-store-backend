a80c94890d8cacc0a91680f572e6987f2a4d8392
devjasim <jasimfbd@gmail.com>
2023-01-20 09:17:28 +0600

    message

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -4,6 +4,6 @@ import userAuth from "../middleware/user.auth.js";
 const router = express.Router();
 
 router.get('/get-products', getProducts);
-router.get('/get-product', getProductById);
+router.get('/get-product/:id', getProductById);
 
 export default router;
